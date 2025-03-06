6d15e9de714309e4621810c5333f71746418b209
Ashley Williams <ashley.williams@strongmind.com>
2025-03-05 19:33:58 -0700

    Implement initial event fetching in App component with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import CitySearch from './components/CitySearch'
 import EventList from './components/EventList'
 import NumberOfEvents from './components/NumberOfEvents'
+import { getEvents } from './api'
 import './App.css'
 
 const App = () => {
   const [events, setEvents] = useState([])
   const [numberOfEvents, setNumberOfEvents] = useState(32)
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const events = await getEvents()
+      setEvents(events)
+    }
+    fetchEvents()
+  }, [])
 
   return (
     <div className="App">
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,7 @@
-import { render } from '@testing-library/react';
+import { render, within, waitFor } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
+import { getEvents } from '../api';
 
 describe('<App /> component', () => {
   test('renders list of events', () => {
@@ -16,5 +17,16 @@ describe('<App /> component', () => {
   test('renders NumberOfEvents', () => {
     const AppDOM = render(<App />).container.firstChild;
     expect(AppDOM.querySelector('.number-of-events')).toBeInTheDocument();
+  });
+
+  test('renders list of events when app is mounted', async () => {
+    const { container } = render(<App />);
+    const eventListDOM = container.querySelector('#event-list');
+    expect(eventListDOM).toBeInTheDocument();
+
+    await waitFor(() => {
+      const eventItems = within(eventListDOM).queryAllByRole('listitem');
+      expect(eventItems.length).toBeGreaterThan(0);
+    });
   });
 }); 
